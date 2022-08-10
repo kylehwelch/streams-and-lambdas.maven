d053d5f7e07f42fdedb0c5dd4b2138ca1c91db30
kylehwelch <welch@udel.edu>
2022-08-10 17:17:26 -0400

    personfactory CHECK

--- src/main/java/com/zipcodewilmington/streams/anthropoid/PersonFactory.java
+++ src/main/java/com/zipcodewilmington/streams/anthropoid/PersonFactory.java
@@ -3,6 +3,7 @@ package com.zipcodewilmington.streams.anthropoid;
 import com.zipcodewilmington.streams.tools.RandomUtils;
 import com.zipcodewilmington.streams.tools.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,7 +40,11 @@ public final class PersonFactory {
      * @return - ArrayList of Person objects
      */ // TODO
     public List<Person> createPersonList(int listSize) {
-        return null;
+        List<Person> plist = new ArrayList();
+        for (int i = 0; i < listSize ; i++) {
+            plist.add(createRandomPerson());
+        }
+        return plist;
     }
 
 
@@ -48,7 +53,8 @@ public final class PersonFactory {
      * @return - Array of Person objects
      */ // TODO
     public Person[] createPersonArray(int arrayLength) {
-        return null;
+
+        return createPersonList(arrayLength).toArray(new Person[0]);
     }
 
 
@@ -59,6 +65,6 @@ public final class PersonFactory {
      * @return - Stream representation of collection of Person objects
      */ // TODO
     public Stream<Person> createPersonStream(int streamCount) {
-        return null;
+        return createPersonList(streamCount).stream();
     }
 }
